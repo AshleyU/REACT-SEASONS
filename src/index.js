0def69edd7dab88836b4658034579c3445246631
Ashley U <ashley.umetsu@gmail.com>
2022-01-06 16:59:58 -0600

    Avoiding conditionals in render

--- src/index.js
+++ src/index.js
@@ -14,16 +14,24 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent () {
         if(this.state.errorMessage && !this.state.lat) {
             return <div><h1>Error: {this.state.errorMessage}</h1></div>;
         }
 
         if(!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
-        }
+        } 
 
-        return <Spinner />;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
